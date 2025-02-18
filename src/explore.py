a0f98cd03c91fe18a35161ef9bb10777dbd84b33
Andrey R <andrey.risukhin@outlook.com>
2025-02-17 20:14:03 -0800

    initial

---
+++ src/explore.py
@@ -0,0 +1,21 @@
+"""
+Explore the schematic files, get out json data about each, in particular the blocks and their positions.
+Format in a clear way to be used by the LLM to understand the schematic.
+"""
+
+import json
+import os
+import litemapy
+
+file = "../schematics/door 2x2.litematic"
+
+def print_schematic_info(file):
+
+
+
+
+
+
+
+
+
